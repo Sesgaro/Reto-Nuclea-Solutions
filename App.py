27c97a8562f9f0bdb45e1aee7478f4b665179abe
Sesgaro <122136427+Sesgaro@users.noreply.github.com>
2023-11-27 00:30:14 -0600

    Add files via upload

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import flet as ft
+import random
 import pandas as pd
 
 def main(page: ft.Page):
@@ -11,51 +12,172 @@ def main(page: ft.Page):
             archivo_seleccionado.value = ', '.join(map(lambda f: f.name,event.files))
         else:
             archivo_seleccionado.value = 'CANCELADO'
+            page.snack_bar = ft.SnackBar(ft.Text("Error, archivo no cargado"))
+            page.snack_bar.open = True
     
         archivo_seleccionado.update()
-        page.snack_bar = ft.SnackBar(ft.Text("Espera un momento, estamos analizando el archivo"))
-        page.snack_bar.open = True
-
-        page.update()
         if archivo_seleccionado.value!='CANCELADO':
+            page.snack_bar = ft.SnackBar(ft.Text("Espera un momento, estamos analizando el archivo"))
+            page.snack_bar.open = True
+            page.update()
+            
             try:
-                datos=pd.read_excel(archivo_seleccionado.value)
-                datos.sort_values(by=['ID','TimeStamp'], ascending=True, inplace=True)
-                global dts_global,posiciones,lista_id
-                dts_global=datos
-                posiciones = []
-                lista_sin_duplicados=[]
-                lista_id=dts_global["ID"].tolist()            
                 
-                for i, elemento in enumerate(lista_id):
-                    if elemento not in lista_sin_duplicados:
-                        lista_sin_duplicados.append(elemento)
-                        posiciones.append(i)
-                b.disabled=False
+                datos=pd.read_excel(archivo_seleccionado.value)
+                datos.sort_values(by=['ID','Fecha','Proyecto'], ascending=True, inplace=True)
+                global dts_global,pos_id,lista_id,color_p_list,color_t_list,lista_id,lista_pro,lista_tl,lista_fecha,lista_sd_pro,lista_sd_tl
+                dts_global=datos
+                
+                pos_id = []
+                pos_pro=[]
+                pos_fe=[]
+                pos_tl=[]            
+                
+                lista_sd_id=[]
+                lista_sd_pro=[]       
+                lista_sd_pro_t=[]                
+                     
+                lista_sd_tl=[]                
+                lista_sd_fe=[]
+                color_t_list=[]
+                color_p_list=[]
+                
+                lista_id=dts_global["ID"].tolist()
+                lista_pro=dts_global["Proyecto"].tolist() 
+                lista_tl=dts_global["Tool"].tolist()
+                lista_fecha=dts_global['Fecha'].tolist()
+                
+                for i, x in enumerate(lista_id):
+                    if x not in lista_sd_id:
+                        lista_sd_id.append(x)
+                        pos_id.append(i)
+                        
+                for i, x in enumerate(lista_pro):
+                    if x not in lista_sd_pro:
+                        lista_sd_pro.append(x)
+                        pos_pro.append(i)
+                        
+                for i, x in enumerate(lista_tl):
+                    if x not in lista_sd_tl:
+                        lista_sd_tl.append(x)
+                        pos_tl.append(i)
+                        
+                for i, x in enumerate(lista_fecha):
+                        if x not in lista_sd_fe:
+                            lista_sd_fe.append(x)
+                            pos_fe.append(i)   
+                            
+                for i, x in enumerate(lista_sd_pro):
+                    
+                    color_ap = '#{:06X}'.format(random.randint(0, 0xFFFFFF))
+                    color_p_list.append(color_ap)
+                
+                for i, x in enumerate(lista_sd_tl):
+                    color_at = '#{:06X}'.format(random.randint(0, 0xFFFFFF))
+                    if color_at not in color_t_list:
+                        color_t_list.append(color_at)
+                
+                
+                    
             except Exception as ex:
                 t.value = str(ex)
+                print(str(ex))
         t.update()
+        grafica.visible=True
+        
+
+        proyectos.update()
+        page.update()
         page.snack_bar = ft.SnackBar(ft.Text("Listo!"))
         page.snack_bar.open = True
-        # page.update()
         return datas(None)
-
+    proyectos=ft.DataTable(
+        border=ft.border.all(2, "#2f2f2f"),
+        border_radius=10,
+        divider_thickness=0,
+        vertical_lines=ft.border.BorderSide(2, "#2f2f2f"),
+        horizontal_lines=ft.border.BorderSide(1, "#2f2f2f"),
+        column_spacing=10,
+        columns=[
+            ft.DataColumn(ft.Text('Fecha')),
+            ft.DataColumn(ft.Text('Proyecto')),
+            ft.DataColumn(ft.Text('Leyenda P')),
+            ft.DataColumn(ft.Text('Herramienta')),
+            ft.DataColumn(ft.Text('Leyenda H')),
+            ft.DataColumn(ft.Text('Magnitud'))
+        ],
+        rows=[]
+    )
     def datas(e):
-        if dts_global is not None:
-            if b.data >= len(posiciones):
-                b.data=0
-            t.value=lista_id[posiciones[b.data]]
-            print(posiciones[b.data])
-            b.data=b.data+1
-            print(b.data)
+        proyectos.rows.clear()
+        proyectos.update()
+        b.disabled=True
+        b.update()
+        try:
+            
+            if dts_global is not None:
+                b.data=b.data+1
+                
+                if b.data == len(pos_id):
+                    b.data=0
+                
+                for i in range(pos_id[b.data], pos_id[b.data+1]):
+                                                
+                    fecha=ft.DataCell(ft.Text(lista_fecha[i]))
+                    proye=ft.DataCell(ft.Text(lista_pro[i])),
+                    tool=ft.DataCell(ft.Text(lista_tl[i]))
+                    for z,x in enumerate(color_p_list):
+                        if lista_pro[i] == lista_sd_pro[z]:
+                            color_p=ft.DataCell(ft.Container(bgcolor=x))
+                            break
+                        else :
+                            color_t=ft.DataCell(ft.Container(bgcolor='black'))
+                    for z,x in enumerate(color_t_list):
+                        if lista_tl[i] == lista_sd_tl[z]:
+                            color_t=ft.DataCell(ft.Container(bgcolor=x))
+                            break
+                        else :
+                            color_t=ft.DataCell(ft.Container(bgcolor='black'))
+                    
+                    proyectos.rows.append(ft.DataRow(
+                            cells=[
+                            ft.DataCell(ft.Text(lista_fecha[i])),
+                            ft.DataCell(ft.Text(lista_pro[i])),
+                            color_p,
+                            ft.DataCell(ft.Text(lista_tl[i])),
+                            color_t,
+                            ft.DataCell(ft.Text('1')),
+                            ]
+                            )
+                        )
+                # aux=cont
+                t.value=lista_id[pos_id[b.data]]
+                print(pos_id[b.data+1])
+                print(b.data)
+        
+                b.disabled=False
+
+                b.update()
+                t.update()
+                proyectos.update()
+                page.update()
+        except Exception as ex:
+            print(str(ex))
+            b.disabled=False
             b.update()
-            t.update()
-            page.update()
+            
+            
 
     archivo_info = ft.FilePicker(on_result=leer_resultados)
     archivo_seleccionado = ft.Text()
     t=ft.Text()
-
+    grafica=ft.Container(
+                visible=False,
+                content=proyectos,
+                bgcolor='#212121',
+                
+                )
+    
     page.overlay.extend([archivo_info])
 
     b=ft.ElevatedButton(
@@ -63,10 +185,11 @@ def main(page: ft.Page):
                     icon=ft.icons.ARROW_BACK,
                     on_click=datas,
                     disabled=True,
-                    data=0
+                    data=-1
                 )
-
+    page.scroll='auto'
     page.add(
+        
         ft.Row(
             [
                 ft.ElevatedButton(
@@ -83,9 +206,9 @@ def main(page: ft.Page):
                 t
             ],
         ),
+        grafica,
         ft.Column(
             [
-            
             ]
         )
     )
