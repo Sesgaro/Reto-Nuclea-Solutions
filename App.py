70a752afa5d5a6bc8be91041961b44854b490acb
Sesgaro <122136427+Sesgaro@users.noreply.github.com>
2023-12-21 01:18:58 -0600

    Update App.py

--- App.py
+++ App.py
@@ -1,15 +1,21 @@
 import flet as ft
+import flet.canvas as cv
+
 from typing import Dict
 import random
 import pandas as pd
+import math
 
-
+emp=1
 
 def main(page: ft.Page):
+    page.bgcolor="#1f262f"
+    page.vertical_alignment="center"
+    page.horizontal_alignment="center"
 
     #Carga de archivos
     def file_picker_result(event: ft.FilePickerResultEvent):
-        btn_subir.current.disabled = True if event.files is None else False
+        # btn_subir.current.disabled = True if event.files is None else False
         barras_progreso.clear()
         archivos.current.controls.clear()
 
@@ -18,10 +24,12 @@ def main(page: ft.Page):
                 pbr_archivo = ft.ProgressRing(value=0, bgcolor='#eeeeee', width=20, height=20)
                 barras_progreso[f.name] = pbr_archivo
                 archivo_seleccionado.value=f.name
-                archivos.current.controls.append(ft.Row([pbr_archivo, ft.Text(f.name)]))
+                items=[pbr_archivo, ft.Text(f.name)]
+                archivos.current.controls.append(ft.Row(items,alignment=ft.MainAxisAlignment.CENTER))
                 # archivo_seleccionado.update()
         
         page.update()
+        return (upload_files(None))
 
     def on_upload_progress(event: ft.FilePickerUploadEvent):
         barras_progreso[event.file_name].value = event.progress
@@ -45,6 +53,7 @@ def main(page: ft.Page):
                 )
             
             file_picker.upload(lista_archivos)
+            
     
     
     #Al momento de cargarse el archivo, se lee y se separan datos en diferentes listas
@@ -57,7 +66,8 @@ def main(page: ft.Page):
                 
                 datos=pd.read_excel(f"uploads/{archivo_seleccionado.value}")
                 datos.sort_values(by=['ID','Fecha','Proyecto'], ascending=True, inplace=True)
-                global dts_global,pos_id,lista_id,color_p_list,color_t_list,lista_id,lista_pro,lista_tl,lista_fecha,lista_sd_pro,lista_sd_tl
+                
+                global dts_global,pos_id,lista_id,color_p_list,color_t_list,lista_id,lista_pro,lista_tl,lista_fecha,lista_sd_pro,lista_sd_tl,mg_list
                 dts_global=datos
                 
                 pos_id = []
@@ -65,9 +75,10 @@ def main(page: ft.Page):
                 pos_fe=[]
                 pos_tl=[]            
                 
+                mg_list=[]
+
                 lista_sd_id=[]
-                lista_sd_pro=[]       
-                lista_sd_pro_t=[]                
+                lista_sd_pro=[]      
                      
                 lista_sd_tl=[]                
                 lista_sd_fe=[]
@@ -83,7 +94,8 @@ def main(page: ft.Page):
                     if x not in lista_sd_id:
                         lista_sd_id.append(x)
                         pos_id.append(i)
-                        
+                pos_id.append(i)
+
                 for i, x in enumerate(lista_pro):
                     if x not in lista_sd_pro:
                         lista_sd_pro.append(x)
@@ -108,122 +120,136 @@ def main(page: ft.Page):
                     color_at = '#{:06X}'.format(random.randint(0, 0xFFFFFF))
                     if color_at not in color_t_list:
                         color_t_list.append(color_at)
+
+                for i, x in enumerate(pos_id):
+                    if i!=len(pos_id)-1:
+                        difer=pos_id[i+1]-pos_id[i]
+                        mg_list.append(difer)
+                    else:
+                        break
+
+                print(mg_list,"\n")
+                ver_emp.disabled=False
                 
                 
                     
         except Exception as ex:
                 t.value = str(ex)
                 print(str(ex))
-        t.update()
-        grafica.visible=True
-        
 
+        t.update()
+        
         proyectos.update()
         page.update()
         page.snack_bar = ft.SnackBar(ft.Text("Listo!"))
         page.snack_bar.open = True
-        return datas(None)
+        page.update()
+
     
     #Se imprimen datos segun cada empleado
-    def datas(e):
-        grafica.visible=False
-        grafica.update()
-        carga.visible=True
-        carga.update()
-        t.visible=False
-        t.update()
-        magnitud=1
-        fecha_t=[]
-        proye_t=[]
-        tool_t=[]
-        fecha_t.append(lista_fecha[pos_id[b.data+1]])
-        proye_t.append(lista_pro[pos_id[b.data+1]])
-        tool_t.append(lista_tl[pos_id[b.data+1]])
-        proyectos.rows.clear()
-        proyectos.update()
-        b.disabled=True
-        b.update()
-        try:
-            
-            if dts_global is not None:
-                b.data=b.data+1
-                
-                if b.data == len(pos_id):
-                    b.data=0
-                
-                for i in range(pos_id[b.data], pos_id[b.data+1]):
-                                                
-                    # fecha=ft.DataCell(ft.Text(lista_fecha[i]))
-                    # proye=ft.DataCell(ft.Text(lista_pro[i])),
-                    # tool=ft.DataCell(ft.Text(lista_tl[i]))
-                    for z,x in enumerate(color_p_list):
-                        if lista_pro[i] == lista_sd_pro[z]:
-                            color_p=ft.DataCell(ft.Container(bgcolor=x))
-                            break
-                        else :
-                            color_t=ft.DataCell(ft.Container(bgcolor='black'))
-                    for z,x in enumerate(color_t_list):
-                        if lista_tl[i] == lista_sd_tl[z]:
-                            color_t=ft.DataCell(ft.Container(bgcolor=x))
-                            break
-                        else :
-                            color_t=ft.DataCell(ft.Container(bgcolor='black'))
+    def datas(e: ft.ContainerTapEvent):
+        global emp
+        for i in range(3):
+            try: 
+                if e.control == flecha_superior:
+                    emp = emp+1
+                    if emp == -1:
+                        emp = emp+1
 
-                    
-                    if lista_fecha[i+1] in fecha_t and lista_pro[i+1] in proye_t and lista_tl[i+1] in tool_t:
-                        magnitud=magnitud+1
-                    else:
-                        proyectos.rows.append(ft.DataRow(
-                            cells=[
-                            ft.DataCell(ft.Text(lista_fecha[i])),
-                            ft.DataCell(ft.Text(lista_pro[i])),
-                            color_p,
-                            ft.DataCell(ft.Text(lista_tl[i])),
-                            color_t,
-                            ft.DataCell(ft.Text(magnitud)),
-                                    ]
-                                )
-                            )
-                        fecha_t=[]
-                        proye_t=[]
-                        tool_t=[]
-                        fecha_t.append(lista_fecha[i+1])
-                        proye_t.append(lista_pro[i+1])
-                        tool_t.append(lista_tl[i+1])
-                        magnitud=1
-                
-                
-                t.value=lista_id[pos_id[b.data]]
-                print(pos_id[b.data+1])
-                print(b.data)
-        
-                b.disabled=False
+                else:
+                    emp = emp-1
+                    if emp == -1:
+                        emp = emp-1
 
-                b.update()
-                t.update()
-                t.visible=True
-                t.update()
-                carga.visible=False
-                carga.update()
-                grafica.visible=True
+                grafica.visible=False
                 grafica.update()
-                page.update()
-        except Exception as ex:
-            print(str(ex))
-            b.disabled=True
-            b.update()
+
+                carga.visible=True
+                carga.update()
+
+                t.visible=False
+                t.update()
+
+                magnitud=1
+
+                fecha_t=[]
+                proye_t=[]
+                tool_t=[]
+
+                fecha_t.append(lista_fecha[pos_id[emp]])
+                proye_t.append(lista_pro[pos_id[emp]])
+                tool_t.append(lista_tl[pos_id[emp]])
+                
+                proyectos.rows.clear()
+                proyectos.update()   
+                
+                if dts_global is not None:
+
+                    if emp == len(pos_id)-1:
+                        emp=0
+                    
+                    for i in range(pos_id[emp], pos_id[emp+1]):
+
+                        for z,x in enumerate(color_p_list):
+                            if lista_pro[i] == lista_sd_pro[z]:
+                                color_p=ft.DataCell(ft.Container(bgcolor=x))
+                                break
+                            else :
+                                color_t=ft.DataCell(ft.Container(bgcolor='black'))
+
+                        for z,x in enumerate(color_t_list):
+                            if lista_tl[i] == lista_sd_tl[z]:
+                                color_t=ft.DataCell(ft.Container(bgcolor=x))
+                                break
+                            else :
+                                color_t=ft.DataCell(ft.Container(bgcolor='black'))
+
+                        
+                        if lista_fecha[i+1] in fecha_t and lista_pro[i+1] in proye_t and lista_tl[i+1] in tool_t:
+                            magnitud=magnitud+1
+                    
+                        else:
+                            proyectos.rows.append(ft.DataRow(
+                                cells=[
+                                ft.DataCell(ft.Text(lista_fecha[i])),
+                                ft.DataCell(ft.Text(lista_pro[i])),
+                                color_p,
+                                ft.DataCell(ft.Text(lista_tl[i])),
+                                color_t,
+                                ft.DataCell(ft.Text(magnitud)),
+                                        ]
+                                    )
+                                )
+                            fecha_t=[]
+                            proye_t=[]
+                            tool_t=[]
+                            fecha_t.append(lista_fecha[i+1])
+                            proye_t.append(lista_pro[i+1])
+                            tool_t.append(lista_tl[i+1])
+                            magnitud=1
+                    
+                    
+                    t.value=f"ID: {lista_id[pos_id[emp]]}"
+                    print(pos_id[emp])
+                    print(emp,"\n")
+            
+                    t.update()
+                    t.visible=True
+                    t.update()
+                    carga.visible=False
+                    carga.update()
+                    grafica.visible=True
+                    grafica.update()
+                    page.update()
+                    ver_emp.visible=False
+                    break
+            except Exception as ex:
+                print(str(ex))
+                emp=1
             
             
 
     #   Se declaran las animaciones, columnas y botones
-
-    b=ft.ElevatedButton(
-        'Siguiente Empleado',
-        icon=ft.icons.ARROW_BACK,
-        on_click=datas,
-        disabled=True,
-        data=-1
-    )
     
     page.scroll='auto'
 
@@ -232,8 +258,6 @@ def main(page: ft.Page):
     barras_progreso: Dict[str, ft.ProgressRing] = {}
 
     archivos = ft.Ref[ft.Column]()
-    
-    btn_subir = ft.Ref[ft.ElevatedButton]()
 
     proyectos=ft.DataTable(
         border=ft.border.all(2, "#2f2f2f"),
@@ -257,50 +281,66 @@ def main(page: ft.Page):
 
     archivo_seleccionado = ft.Text()
     
-    t=ft.Text()
+    t=ft.Text(size=30)
     
     carga=ft.ProgressRing(visible=False)
+    
+    select_btn = ft.ElevatedButton('Selecciona un archivo', icon=ft.icons.FOLDER_OPEN, on_click=lambda _: file_picker.pick_files(allow_multiple=False,allowed_extensions=["xls","xlsx"]))
+    
+    columna_archivos = ft.Column(ref=archivos,horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+
+    concarga=ft.Container(alignment=ft.alignment.center,height=300,content=carga)
+
+    triangulo = cv.Canvas([
+        cv.Path(
+                [   
+                    ft.canvas.Path.MoveTo(40, 0),
+                    ft.canvas.Path.LineTo(0,50),
+                    ft.canvas.Path.LineTo(80,50),
+                    cv.Path.Close(),
+                ],
+                paint=ft.Paint(
+                stroke_width=4,
+                style=ft.PaintingStyle.STROKE,
+                gradient=ft.LinearGradient(
+                    begin=ft.alignment.top_left,
+                    end=ft.Alignment(0,30),
+                    colors=[
+                        "#5a5bf3",
+                        "#91e7d9",
+                    ],
+                    tile_mode=ft.GradientTileMode.MIRROR,
+                ),
+                ),
+            ),
+        ],
+        width=80,
+        height=50,
+    )
+
+    
+    flecha_superior = ft.Container(triangulo, width=80, height=50, on_click=datas,rotate=ft.Rotate(angle=math.pi/2))
+
+    items_grafica=[ft.Container(triangulo, width=80, height=50, rotate=ft.Rotate(angle=-math.pi/2), on_click=datas),proyectos,flecha_superior]
+
+    ver_emp=ft.ElevatedButton('Ver empleados',disabled=True,on_click=datas)
 
     grafica=ft.Container(
         visible=False,
         alignment=ft.alignment.center,
-        content=ft.Row([proyectos,carga,]),
-        bgcolor='#212121',        
+        content=ft.Row(items_grafica,alignment=ft.MainAxisAlignment.CENTER)  
         )
     
-    page.overlay.extend([archivo_info])
+    best_txt=ft.Text()
+
+    best_emp=ft.Container(opacity=0,content=best_txt
+                          )
+    col = ft.Column(spacing=10,horizontal_alignment=ft.CrossAxisAlignment.CENTER,controls=[select_btn, columna_archivos, ver_emp, t, grafica, concarga])
+
+    contenedor = ft.Container(col, alignment=ft.alignment.center, margin=0,)
     
     #Se inicializa la ventana
     page.add(
-        
-        ft.Row(
-            [
-                ft.ElevatedButton(
-            'Seleccionar archivos...',
-            icon=ft.icons.FOLDER_OPEN,
-            on_click=lambda _: file_picker.pick_files(allow_multiple=False,allowed_extensions=["xls","xlsx"])
-        ),
-        ft.Column(ref=archivos),
-            ft.ElevatedButton(
-                    'Subir',
-                    ref=btn_subir,
-                    icon=ft.icons.UPLOAD,
-                    on_click=upload_files,
-                    disabled=True
-                )
-            ]
-        ),
-        ft.Row(
-            [
-                b,  
-                t
-            ],
-        ),
-        grafica,
-        carga,
-        ft.Column(
-            [
-            ]
-        )
+        contenedor
     )
-ft.app(target=main, view=ft.AppView.WEB_BROWSER, web_renderer=ft.WebRenderer.HTML, upload_dir='uploads')
+ft.app(target=main, view=ft.AppView.WEB_BROWSER, upload_dir='uploads')
